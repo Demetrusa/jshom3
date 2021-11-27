d7b21a127771bb056de14a5b9e39976875a8d94f
Demetrusa <demetre.kurdadze20b@geolab.edu.gegit config --global user.name Demetrusacd /Desktop/Users/Git/Demegit config --global user.email demetre.kurdadze20b@geolab.edu.ge>
2021-11-27 19:16:38 +0400

    homework

---
+++ app.js
@@ -0,0 +1,18 @@
+
+let arr = [14, 2, 45, 13, 54, 66, 21, 40, -2, 15, 187, -300, 200];
+
+console.log(Math.min(...arr))
+console.log(Math.max(...arr))
+
+function numbersMinMax(a,b){
+    return a*b;
+}
+
+
+
+// let sNumber = Math.pow(5,2);
+// console.log("5*5 = ",squarNumber);
+
+// let variable = 5 ;
+// let sNumber2 = Math.pow(variable,2);
+// console.log("5*5 = ",sNumber2);
